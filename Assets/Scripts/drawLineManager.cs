e1bd9b7b474fb8a9c27bc84e0ea9745993b17ddc
lvingonzales <lvingonzales15@gmail.com>
2023-12-04 14:34:30 -0500

    color (Working)

--- Assets/Scripts/drawLineManager.cs
+++ Assets/Scripts/drawLineManager.cs
@@ -5,13 +5,26 @@ using UnityEngine;
 public class drawLineManager : MonoBehaviour
 {
     // Update is called once per frame
+
+    void Start()
+    {
+        
+    }
     void Update()
     {
         CheckButtonInputs();
     }
 
+    public Material mat1;
+    public Material mat2;
+    public Material mat3;
+    public Material mat4;
+    public Material mat5;
+    public Material mat6;
     int numClick = 0;
+    int currColor = 1;
     private LineRenderer currLine;
+
     private void CheckButtonInputs()
     {
         OVRInput.Update();
@@ -24,8 +37,38 @@ public class drawLineManager : MonoBehaviour
             GameObject line = new GameObject ();
             currLine = line.AddComponent<LineRenderer> ();
 
-            currLine.startWidth = 0.1f;
-            currLine.endWidth = 0.1f;
+            //added
+            //lineRenderer = currLine.GetComponent<LineRenderer>();
+            /*currLine.startColor = Color.blue;
+            currLine.endColor = Color.red;*/    
+            //
+
+            //currLine.GetComponent<LineRenderer>().material = mat1;
+
+            switch (currColor)
+            {
+            case 1:
+                currLine.GetComponent<LineRenderer>().material = mat1;
+            break;
+            case 2:
+                currLine.GetComponent<LineRenderer>().material = mat2;
+            break;
+            case 3:
+                currLine.GetComponent<LineRenderer>().material = mat3;
+            break;
+            case 4:
+                currLine.GetComponent<LineRenderer>().material = mat4;
+            break;
+            case 5:
+                currLine.GetComponent<LineRenderer>().material = mat5;
+            break;
+            case 6:
+                currLine.GetComponent<LineRenderer>().material = mat6;
+            break;
+            }
+
+            currLine.startWidth = 0.01f;
+            currLine.endWidth = 0.01f;
             
             numClick = 0;
         }else if(OVRInput.Get(OVRInput.Button.One))
@@ -34,7 +77,28 @@ public class drawLineManager : MonoBehaviour
             Debug.Log ("A is being held");
             currLine.SetPosition (numClick, rightControllerPosition);
             numClick++;
+        } else if (OVRInput.GetDown(OVRInput.Button.Two))
+        {
+            Debug.Log ("B was pressed");
+            currColor+=1;
+            if (currColor > 6)
+            {
+                currColor = 1;
+            }
         }
+
+        /*OVRInput.Update();
+        if (OVRInput.GetDown(OVRInput.Button.Two))
+        {
+            Debug.Log ("B was pressed");
+            currColor+=1;
+            if (currColor > 6)
+            {
+                currColor = 1;
+            }
+
+
+        }*/
 
     }
 }
